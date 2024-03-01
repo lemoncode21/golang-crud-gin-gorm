c1a76e2c1743382a5a6ad2b584adcae3b69d9da1
Pedro Medina Faria <pedromedina190@gmail.com>
2024-02-29 22:08:49 -0300

    begin implementing sessions

--- model/users.go
+++ model/users.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model        // Adds fields ID, CreatedAt, UpdatedAt, DeletedAt
-	Email      string `gorm:"type:varchar(100);unique_index"`
+	Email      string `gorm:"type:varchar(100);uniqueIndex"`
 	Name       string `gorm:"type:varchar(100)"`
 	Password   string `gorm:"type:varchar(255)"` // Store hashed password
 }
